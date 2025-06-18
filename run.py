c3d7fb43c381d88b8b624adf6d8ed8cb2f0c4c1b
Ayush Chorghe <ayushchorge123@gmail.com>
2025-06-18 17:48:55 +0530

    end of errors

--- run.py
+++ run.py
@@ -1,10 +1,9 @@
 from app import create_app
 from app.extensions import db
-from app.models import Person, Expense, Group  # Ensure all your models are imported
+from app.models import Person, Expense, Group  # Import all models
 
 app = create_app()
 
-if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()  # This will create all tables if they don't exist
-    app.run(host="0.0.0.0", port=5000)
+# Run once to create tables
+with app.app_context():
+    db.create_all()
